664222a68c48e01dcad4dfeccc4adb0d8175a897
amaanabdullah16932-art <amaanabdullah.16932@gmail.com>
2025-11-03 15:47:41 +0530

    message

---
+++ main.py
@@ -0,0 +1,7 @@
+num1 = float(input("enter no."))
+num2= float(input("enter 2 nd no,"))
+addition = num1 + num2
+substraction = num1 - num2
+product = num1*num2
+division = num1/num2
+print("add ans:-",addition,"sub ans:-",substraction,"product ans:-",product,"division ans:-",division)
