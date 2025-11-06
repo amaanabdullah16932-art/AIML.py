3df1fff43cf9b9b8924ba444de3bd80de3c862b9
amaanabdullah16932-art <amaanabdullah.16932@gmail.com>
2025-11-06 15:46:12 +0530

    updated formatting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 name = "AMAAN ABDULLAH"
 AGE=19
 SCHOOL = "METHODIST HIGH SCHOOL"
-print(f"my name is {name} and i am {AGE} years old. I completed my 12 th from {SCHOOL}")
+height=173
+print(f"my name is {name} and i am {AGE} years old. I completed my 12 th from {SCHOOL} and my height is {height} cm")
